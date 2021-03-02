5b9ec53e361ead3d243528fe1d9e7c21f355f6a4
EvgeniyKhodos <en-hodos@mail.ru>
2021-03-02 16:37:22 +0700

    Hello world

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
-"use strict"
+"use strict";
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
 
 app.listen(8080, '0.0.0.0', () => {
     console.log('Server started on port 8080')
-})
+});
